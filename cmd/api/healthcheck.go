bd3035eb4e35148eeea8db513595f79d1190230c
ryan <ryanyogan@gmail.com>
2021-05-21 16:37:17 -0500

    envelope responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthchekHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environemnt": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered an error", http.StatusInternalServerError)
+	}
 }
