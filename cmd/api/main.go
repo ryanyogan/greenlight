7a5b4dabce12504e6a3bc22fa74578125c5fbb00
ryan <ryanyogan@gmail.com>
2021-05-26 14:26:09 -0500

    pull in flag vars to override db settings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,7 +19,10 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn string
+		dsn          string
+		maxOpenConns int
+		maxIdelConns int
+		maxIdelTime  string
 	}
 }
 
@@ -34,6 +37,9 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment to run under")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgrSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PG max open connections")
+	flag.IntVar(&cfg.db.maxIdelConns, "db-max-idle-conns", 25, "PG max idle connections")
+	flag.StringVar(&cfg.db.maxIdelTime, "db-max-idle-time", "15m", "PG max idle time")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -69,6 +75,15 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	duration, err := time.ParseDuration(cfg.db.maxIdelTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdelConns)
+	db.SetConnMaxIdleTime(duration)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
