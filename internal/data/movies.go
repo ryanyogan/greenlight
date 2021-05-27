61a6033d95a5bc954a1784cfebe21304566a3899
ryan <ryanyogan@gmail.com>
2021-05-27 18:28:16 -0500

    added vectoring and GIN indices for text search

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -90,10 +90,12 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
+	// This query is Postgres specific, it involves vectoring for
+	// full-text search, and filtering. Please adapt based on data store.
 	query := `
 	SELECT id, created_at, title, year, runtime, genres, version
 	FROM movies
-	WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+	WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 	AND (genres @> $2 OR $2 = '{}')
 	ORDER BY id`
 
