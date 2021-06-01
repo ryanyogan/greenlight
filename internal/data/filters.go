37e48ca218e5628f630a1d9abd874efa9e8aa259
ryan <ryanyogan@gmail.com>
2021-06-01 15:04:29 -0500

    sorting by qs params

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "yogan.dev/greenlight/internal/validator"
+import (
+	"strings"
+
+	"yogan.dev/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -15,4 +19,22 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than 0")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort paramater " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
